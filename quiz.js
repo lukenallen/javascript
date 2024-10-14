7911350c5c963a94fb50d7f7594f7a57fc26beb8
[luke allen] <[e3682799@student.dcccd.edu]>
2024-10-13 19:25:10 -0500

    init commit

--- quiz.js
+++ quiz.js
@@ -1,12 +1,12 @@
-var mathQuest = ['3 + 2', '7 - 4', '10 / 5', '6 X 2'];
-var mathAns = ['5', '3', '2', '12'];
-function mathQuiz() {
+var basicQuiz = ['What is the opposite of Left?', 'What is the opposite of Down?', 'What is the opposite of Off?'];
+var basicAns = ['Right', 'Up', 'On'];
+function basicQuiz() {
   var points = 0;
   for (i = 0; i <= 3; i++) {
     var attempts = 3;
     while (attempts > 0) {
-      ans = prompt("What is " + mathQuest[i]);
-      if (ans == mathAns[i]) {
+      ans = prompt("What is " + basicQuiz[i]);
+      if (ans == basicAns[i]) {
         points = points + 1;
         alert("Correct!");
         attempts = 0;
