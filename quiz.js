b46bad349e6956a84f9c4852e2937da65dcbbe58
[luke allen] <[e3682799@student.dcccd.edu]>
2024-10-13 21:55:20 -0500

    init commit

--- quiz.js
+++ quiz.js
@@ -1,21 +1,33 @@
-var points = 0;
-var questions = [
-['What is the opposite of Left?', Right],
-['What is the opposite of Down?', Up],
-['What is the opposite of Off?', On]
-];
-for (var i=0; i<Questions.length; i++) {
-    askQuestion(questions[i]);
-for (var i=0; i<questions.length; i++) {
-    askQuestion(questions[i]);
-}
-function askQuestion(question) {
-    var answer = prompt(question[0],'');
-    if (answer == question[1]) {
-        alert('Correct, Good Job!');
-        points++;
-    } else {
-        alert('Incorrect, Try Again!');
-    }
-    }
-}
+// set up question and matching answer arrays
+var basicQuest = ['What is the opposite of Left?', 'What is the opposite of Down?', 'What is the opposite of Off?'];
+var basicAns = ['Right', 'Up', 'On'];
+
+// math quiz function displays math questions and gives the user 2 attempts to get it right - then returns a score
+function basicQuiz() {
+  // initialize points
+  var points = 0;
+  //use a for loop to loop through each math question
+  for (i = 0; i <= 3; i++) {
+    //set attempts to 3
+    var attempts = 3;
+    // use a while loop to give the user 3 attempts to answer
+    while (attempts > 0) {
+      // prompt user with math question
+      ans = prompt(basicQuest[i]);
+      // see if the answer matches the answer array
+      if (ans == basicAns[i]) {
+        // if correct, add 1 to points, alert user and set attempts to 0
+        points = points + 1;
+        alert("Correct, Good Job!");
+        attempts = 0;
+      } else {
+        // if answer doesn't match, alert user and subtract 1 from attempts
+        alert("Incorrect, Try Again");
+        attempts = attempts - 1;
+        points = points -1;
+      } // end if
+    } // end while
+  } // end for
+  // return the points variable
+  return points;
+} // end basicQuiz function
