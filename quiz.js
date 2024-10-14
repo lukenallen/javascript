fec9b464e01cc237b1ff0ad0208511da4bf0be1f
[luke allen] <[e3682799@student.dcccd.edu]>
2024-10-13 20:45:20 -0500

    init commit

--- quiz.js
+++ quiz.js
@@ -1,38 +1,31 @@
 var basicQuest = ['What is the opposite of Left?', 'What is the opposite of Down?', 'What is the opposite of Off?'];
 var basicAns = ['Right', 'Up', 'On'];
 
+// math quiz function displays math questions and gives the user 2 attempts to get it right - then returns a score
 function basicQuiz() {
-  var points = 0;
-  var attempts = 3;
-    while (attempts > 0) {
-
-      ans = prompt(basicQuest[0]);
-      if (ans == basicAns[0]) {
-        points = points + 1;
-        alert("Correct, Good Job!");
-        attempts = 0;
-      } else {
-        alert("Incorrect, Try Again!");
-        attempts = attempts - 1;
-        ans = prompt(basicQuest[1]);
-      if (ans == basicAns[1]) {
-        points = points + 1;
-        alert("Correct, Good Job!");
-        attempts = 0;
-      } else {
-        alert("Incorrect, Try Again!");
-        attempts = attempts - 1;
-        ans = prompt(basicQuest[2]);
-      if (ans == basicAns[2]) {
-        points = points + 1;
-        alert("Correct, Good Job!");
-        attempts = 0;
-      } else {
-        alert("Incorrect, Try Again!");
-        attempts = attempts - 1;
-        }
-      }
-    }
-  }
-  return points;
-}
+    // initialize points
+    var points = 0;
+    //use a for loop to loop through each math question
+    for (i = 0; i <= 3; i++) {
+      //set attempts to 3
+      var attempts = 3;
+      // use a while loop to give the user 3 attempts to answer
+      while (attempts > 0) {
+        // prompt user with math question
+        ans = prompt("What is " + basicQuest[i]);
+        // see if the answer matches the answer array
+        if (ans == basicAns[i]) {
+          // if correct, add 1 to points, alert user and set attempts to 0
+          points = points + 1;
+          alert("Correct, Good Job!");
+          attempts = 0;
+        } else {
+          // if answer doesn't match, alert user and subtract 1 from attempts
+          alert("Incorrect, Try Again!");
+          attempts = attempts - 1;
+        } // end if
+      } // end while
+    } // end for
+    // return the points variable
+    return points;
+  } // end mathQuiz function
