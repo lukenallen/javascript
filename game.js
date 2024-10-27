6e7a278f5936b190cb3772aec7ccd863767f9ffd
[luke allen] <[e3682799@student.dcccd.edu]>
2024-10-26 19:29:20 -0500

    init commit

---
+++ game.js
@@ -0,0 +1,7 @@
+/* Question Multi-dimensional array */
+var questions = [
+    ["What is the opposite of left?", Correct answer index [int], "Right", "Up", "On"],
+    ["What is the opposite of down?", Correct answer index [int], "On", "Up", "Right"],
+    ["What the opposite of off?", Correct answer index [int], "Up", "Right", "On"]
+ ];
+ questions.shift();
